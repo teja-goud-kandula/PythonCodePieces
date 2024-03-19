13fca1259e97ced855ea1744cf5202f6751f0231
Teja Goud Kandula <kandulateja6@gmail.com>
2024-03-19 20:56:12 +0530

    Create CSVToParquetConversion.py

---
+++ ParquetFiles/CSVToParquetConversion.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+csv_file_path = "/home/teja/Documents/Python/customers-100000.csv"
+df = pd.read_csv(csv_file_path)
+
+table = pa.Table.from_pandas(df)
+
+parquet_file_path = "/home/teja/Documents/Python/data-100000.parquet"
+pq.write_table(table, parquet_file_path)
